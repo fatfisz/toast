4b9dd8160507fbf9b5415da20d0cd74e958c5889
fatfisz <fatfisz@gmail.com>
2019-09-13 01:03:33 +0200

    Misc

--- src/Engine.ts
+++ src/Engine.ts
@@ -24,26 +24,45 @@ interface Drawable {
   draw(display: Display): void;
 }
 
+type Phase = 'intro' | 'intro-fade' | 'playing';
+
+const startScreenPeriod = 60000;
+const startScreenSpeed = 0.5;
 const fadeDuration = 400;
 const darkDuration = 100;
 const fadeAndDarkDuration = fadeDuration + darkDuration;
-const restartPeriod = 800;
+const actionTextPeriod = 800;
+const actionTextWaitingTime = actionTextPeriod * 4;
 const cameraOffset = displaySize / 15;
 const cameraEnd = plateDepth - displaySize / 2 + 20 * imageScale;
 const infoPadding = 30;
 
+const instructions = [
+  'click and drag to cast magick on a spinning toast!',
+  '',
+  'the bigger the move, the more force you will unleash!',
+  '',
+  'there are only two objectives:',
+  ' 1. the toast has to land butter side up',
+  ' 2. the toast has to land in the middle',
+].join('\n');
+
 export default class Engine {
+  actionTextTimestamp!: number;
   display: Display;
   drawables!: Drawable[];
   finishingLine!: FinishingLine;
-  finishTimestamp!: number;
+  minOverlayOpacity: number;
   mouse: Mouse;
   needToInit: boolean;
+  nextPhase: Phase;
   now: number;
   overlayOpacity: number;
-  resetTimestamp: number;
+  overlayTimestamp: number;
+  phase: Phase;
   scoring!: Scoring;
-  showRestartText!: boolean;
+  showActionText!: boolean;
+  startTimestamp: number;
   toast!: Toast;
   wizard!: Wizard;
 
@@ -55,39 +74,84 @@ export default class Engine {
     });
 
     this.display = display;
+    this.minOverlayOpacity = 0;
     this.mouse = mouse;
     this.needToInit = false;
+    this.nextPhase = 'intro-fade';
     this.now = now;
     this.overlayOpacity = 1;
-    this.resetTimestamp = now - fadeDuration;
+    this.overlayTimestamp = 0;
+    this.phase = 'intro';
+    this.startTimestamp = now;
     this.init();
   }
 
   init() {
     this.display.resetCameraPosition();
-    this.finishTimestamp = Infinity;
-    this.showRestartText = false;
+    this.actionTextTimestamp = Infinity;
+    this.showActionText = false;
 
     this.finishingLine = new FinishingLine();
     this.scoring = new Scoring();
     this.toast = new Toast(0, startDepth, this.scoring.highscore);
     this.wizard = new Wizard();
 
-    this.drawables = [
-      this.wizard,
-      new AirFlow(),
-      new AirFlow(2),
-      new AirFlow(3),
-      this.toast,
-      this.finishingLine,
-      this.scoring,
-      this.mouse,
-    ];
+    this.drawables =
+      this.phase === 'intro'
+        ? [new AirFlow(), new AirFlow(2), new AirFlow(3)]
+        : [
+            this.wizard,
+            new AirFlow(),
+            new AirFlow(2),
+            new AirFlow(3),
+            this.toast,
+            this.finishingLine,
+            this.scoring,
+            this.mouse,
+          ];
   }
 
   tick(now: number, dt: number) {
+    if (this.phase !== 'playing') {
+      this.tickIntro(now);
+    } else {
+      this.tickPlaying(now, dt);
+    }
+  }
+
+  private tickIntro(now: number) {
     this.now = now;
-    this.resetTick();
+    const timeFromStart = now - this.startTimestamp;
+
+    if (this.phase === 'intro') {
+      this.overlayTimestamp =
+        roundToSteps(timeFromStart, startScreenPeriod) -
+        (fadeDuration + darkDuration / 2) +
+        this.startTimestamp;
+    }
+    this.overlayTick();
+    if (this.phase === 'intro') {
+      this.overlayOpacity = this.overlayOpacity * 0.4 + 0.6;
+    }
+
+    this.display.setCameraPosition(
+      new Point(0, (timeFromStart % startScreenPeriod) * startScreenSpeed),
+    );
+
+    if (this.phase === 'intro') {
+      this.tickActionText(true, () => {
+        this.actionTextTimestamp = Infinity;
+        this.phase = this.nextPhase;
+        this.nextPhase = 'playing';
+        this.showActionText = false;
+        this.reset();
+      });
+    }
+  }
+
+  private tickPlaying(now: number, dt: number) {
+    this.now = now;
+    this.overlayTick();
     this.toast.tick(dt);
     this.finishingLine.tick(now);
     this.scoring.tick(now, this.toast);
@@ -95,42 +159,47 @@ export default class Engine {
     this.mouse.tick(now, dt, this.display.getOffset(), this.toast);
     this.wizard.tick(now, this.toast.canApplyForce() && this.mouse.pressed);
 
-    if (isFinite(this.finishTimestamp)) {
-      this.showRestartText =
-        now >= this.finishTimestamp &&
-        Math.floor((now - this.finishTimestamp) / restartPeriod) % 2 === 0;
-    } else if (this.toast.phase === 'resting') {
-      const restartWaitingTime = restartPeriod * 5;
-      this.finishTimestamp = now + restartWaitingTime;
+    this.tickActionText(this.toast.phase === 'resting', () => {
+      this.reset();
+    });
+  }
+
+  private tickActionText(trigger: boolean, callback: () => void) {
+    if (isFinite(this.actionTextTimestamp)) {
+      this.showActionText =
+        this.now >= this.actionTextTimestamp &&
+        Math.floor((this.now - this.actionTextTimestamp) / actionTextPeriod) % 2 === 0;
+    } else if (trigger) {
+      this.actionTextTimestamp = this.now + actionTextWaitingTime;
       setTimeout(() => {
-        this.setMouseDownCallback(() => {
-          this.reset();
-        });
-      }, restartWaitingTime);
+        this.setMouseDownCallback(callback);
+      }, actionTextWaitingTime);
     }
   }
 
   private reset() {
-    this.resetTimestamp = this.now;
+    this.overlayTimestamp = this.now;
+    this.minOverlayOpacity = this.overlayOpacity;
     this.needToInit = true;
   }
 
-  private resetTick() {
-    if (this.resetTimestamp + fadeAndDarkDuration < this.now) {
+  private overlayTick() {
+    if (this.overlayTimestamp + fadeAndDarkDuration < this.now) {
       this.overlayOpacity = roundToSteps(
-        Math.max(1 - (this.now - this.resetTimestamp - fadeAndDarkDuration) / fadeDuration, 0),
+        Math.max(1 - (this.now - this.overlayTimestamp - fadeAndDarkDuration) / fadeDuration, 0),
         0.1,
       );
-    } else if (this.resetTimestamp + fadeDuration < this.now) {
+    } else if (this.overlayTimestamp + fadeDuration < this.now) {
       this.overlayOpacity = 1;
       if (this.needToInit) {
+        this.phase = this.nextPhase;
         this.init();
         this.needToInit = false;
       }
     } else {
-      this.overlayOpacity = roundToSteps(
-        Math.max((this.now - this.resetTimestamp) / fadeDuration, 0),
-        0.1,
+      this.overlayOpacity = Math.max(
+        roundToSteps(Math.max((this.now - this.overlayTimestamp) / fadeDuration, 0), 0.1),
+        this.minOverlayOpacity,
       );
     }
   }
@@ -149,24 +218,75 @@ export default class Engine {
   }
 
   draw() {
+    if (this.phase !== 'playing') {
+      this.drawIntro();
+    } else {
+      this.drawPlaying();
+    }
+  }
+
+  private drawIntro() {
     drawBackground(this.display);
 
     for (const drawable of this.drawables) {
       drawable.draw(this.display);
     }
 
-    this.drawInfo();
+    this.drawOverlay();
+    this.drawIntroInfo();
+  }
 
-    if (this.overlayOpacity > 0) {
-      this.display.rect(new Point(0, 0), new Point(displaySize, displaySize), {
-        absolute: true,
-        fillStyle: getColor(0),
-        globalAlpha: this.overlayOpacity,
-      });
+  private drawIntroInfo() {
+    this.display.reset();
+
+    drawText(
+      this.display.context,
+      `highscore: ${this.scoring.highscore}`,
+      getColorTuple(6),
+      infoPadding,
+      infoPadding,
+      displaySize - infoPadding,
+      { right: true },
+    );
+
+    drawText(this.display.context, 'space wizard', getColorTuple(6), 150, 70, displaySize, {
+      scale: 10,
+    });
+
+    drawText(this.display.context, 'toast spinning', getColorTuple(6), 0, 140, displaySize - 150, {
+      right: true,
+      scale: 10,
+    });
+
+    drawText(this.display.context, instructions, getColorTuple(6), 100, 350, displaySize - 100, {
+      scale: 5,
+    });
+
+    if (this.showActionText) {
+      drawText(
+        this.display.context,
+        'click to start',
+        getColorTuple(6),
+        50,
+        910,
+        displaySize - 50,
+        { center: true, scale: 6 },
+      );
     }
   }
 
-  private drawInfo() {
+  private drawPlaying() {
+    drawBackground(this.display);
+
+    for (const drawable of this.drawables) {
+      drawable.draw(this.display);
+    }
+
+    this.drawPlayingInfo();
+    this.drawOverlay();
+  }
+
+  private drawPlayingInfo() {
     this.display.reset();
 
     const distance = Math.round(Math.min(Math.max(this.toast.mid.y / finishDepth, 0), 1) * 100);
@@ -189,7 +309,7 @@ export default class Engine {
       { right: true },
     );
 
-    if (this.showRestartText) {
+    if (this.showActionText) {
       drawText(
         this.display.context,
         'click to restart',
@@ -199,6 +319,16 @@ export default class Engine {
         displaySize - infoPadding,
         { center: true, scale: 6 },
       );
+    }
+  }
+
+  private drawOverlay() {
+    if (this.overlayOpacity > 0) {
+      this.display.rect(new Point(0, 0), new Point(displaySize, displaySize), {
+        absolute: true,
+        fillStyle: getColor(0),
+        globalAlpha: this.overlayOpacity,
+      });
     }
   }
 
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import Display from './Display';
 import Mouse from './Mouse';
 import startGameLoop from './startGameLoop';
 
+document.title = 'Space Wizard Toast Spinning';
 document.documentElement.style.background = getColor(1);
+
 const display = new Display();
 const mouse = new Mouse();
 mouse.init(display);
